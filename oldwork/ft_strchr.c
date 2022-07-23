34e705368d95149c4167232d958eb2c1edfb0623
Saifa Meethong <smeethon@z3t5c1.42bangkok.com>
2022-07-24 03:14:43 +0700

    add like a yoloo

---
+++ oldwork/ft_strchr.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smeethon <smeethon@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/24 02:50:08 by smeethon          #+#    #+#             */
+/*   Updated: 2022/07/24 03:14:18 by smeethon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include "libft.h"
+#include <stdlib.h>
+
+char	*ft_strchr(const char *s, int c)
+{
+	while (*s)
+	{
+		if (*s == c)
+			return ((char *)s);
+		s++;
+	}
+	return (NULL);
+}
+/*
+#include <stdio.h>
+#include <string.h>
+
+int main () {
+   const char str[] = "http://www.tutorialspoint.com";
+   const char ch = '.';
+   char *ret;
+
+   ret = strchr(str, ch);
+
+   printf("String after |%c| is - |%s|\n", ch, ret);
+   
+   return(0);
+}*/
+/*
+    strchr use for locate character in str (convert c to char) there for 
+    if c is '\0' func will locate terminater '\0'
+*/
+// line 16 -> man strchr
+// line 23 -> convert const char *s to char *s to return
+//include libft for NULL
