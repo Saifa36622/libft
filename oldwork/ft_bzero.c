0571df7ef3b963ffda9b935c03844f2407462a24
Saifa Meethong <smeethon@z3t5c1.42bangkok.com>
2022-07-24 01:56:44 +0700

    add com in bzero

--- oldwork/ft_bzero.c
+++ oldwork/ft_bzero.c
@@ -6,7 +6,7 @@
 /*   By: smeethon <smeethon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/24 01:37:28 by smeethon          #+#    #+#             */
-/*   Updated: 2022/07/24 01:51:59 by smeethon         ###   ########.fr       */
+/*   Updated: 2022/07/24 01:56:15 by smeethon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ void	ft_bzero(void *s, size_t n)
 bzeo is fuc that use to erase data in n byte of the mem start at s and 
 (over write or write ???) that mem by '\0' 
 */
-// line 16 -> man memset fuc 
+// line 16 -> man bzero fuc 
 // line 22 -> change s to unsigned char to use in compare line (bzero -
 // require no return)
 // line 28 -> can compare s to y because y and s is unsigned char 
