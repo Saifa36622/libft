4145f477efc9ff9c6c479bd2ae90b73f3b52517c
Saifa Meethong <smeethon@z3t5c1.42bangkok.com>
2022-07-23 10:29:27 +0700

    add alnum

--- oldwork/ft_isdigit.c
+++ oldwork/ft_isdigit.c
@@ -1,33 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_str_is_alpha.c                                  :+:      :+:    :+:   */
+/*   ft_isdigit.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: smeethon <smeethon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/07/23 09:44:48 by smeethon          #+#    #+#             */
-/*   Updated: 2022/07/23 10:11:26 by smeethon         ###   ########.fr       */
+/*   Created: 2022/07/23 10:18:23 by smeethon          #+#    #+#             */
+/*   Updated: 2022/07/23 10:28:02 by smeethon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
-int	ft_str_is_alpha(char *str)
+int		ft_isdigit(int x)
 {
-	int	x;
-
-	x = 0;
-	if (str[x] == '\0')
-	{
+	if (x >= '0' && x <= '9')
 		return (1);
-	}
-	while (str[x])
-	{
-		if ((str[x] >= 'A' && str[x] <= 'Z') || \
-				(str[x] >= 'a' && str[x] <= 'z'))
-			x++;
-		else
-			return (0);
-	}
-	return (1);
+	return (0);
 }
+/*
+int main ()
+{
+    	printf("%d", ft_isdigit('2s'));
+}*/
