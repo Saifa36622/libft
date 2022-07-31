2771cf8aca1fae1c2e0b46de6f3e0ca647e67b2c
Saifa Meethong <smeethon@z3t5c1.42bangkok.com>
2022-07-31 23:13:14 +0700

    noooo

--- oldwork/ft_isprint.c
+++ oldwork/ft_isprint.c
@@ -1,36 +1,48 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_str_is_alpha.c                                  :+:      :+:    :+:   */
+/*   ft_isprint.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: smeethon <smeethon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/07/23 09:44:48 by smeethon          #+#    #+#             */
-/*   Updated: 2022/07/23 11:48:18 by smeethon         ###   ########.fr       */
+/*   Created: 2022/07/23 11:44:41 by smeethon          #+#    #+#             */
+/*   Updated: 2022/07/31 23:12:57 by smeethon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 //#include <stdio.h>
 
-int	ft_str_is_alpha(char *str)
+/*int	ft_isprint(char *str)
 {
 	int	x;
 
 	x = 0;
 	if (str[x] == '\0')
-		return (1);
-	while (str[x])
 	{
-		if ((str[x] >= 'A' && str[x] <= 'Z') || \
-				(str[x] >= 'a' && str[x] <= 'z'))
+		return (1);
+	}
+	while (str[x] != '\0')
+	{
+		if (str[x] >= 32 && str[x] <= 126)
 			x++;
-		return (0);
+		else
+		{
+			return (0);
+		}
 	}
 	return (1);
-}
-/*
-int main ()
+}*/
+#include "libft.h"
+
+int	ft_isprint(int c)
 {
-	ft_str_is_alpha("hello");
+	if (c < 127 && c >= 32)
+		return (1);
+	return (0);
 }
-*/
+/* 
+int	main()
+{
+	printf("%d\n", ft_str_is_printable("d5r67tyuihgfebhvg"));
+	printf("%d", ft_str_is_printable("\n\t\v\f"));
+} */
