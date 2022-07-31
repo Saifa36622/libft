753d71b949065f265203747d8d2a2988c1f6907c
Saifa Meethong <smeethon@z3t5c1.42bangkok.com>
2022-07-31 23:56:58 +0700

    add header file

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: smeethon <smeethon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/24 12:30:45 by smeethon          #+#    #+#             */
-/*   Updated: 2022/07/31 23:17:22 by smeethon         ###   ########.fr       */
+/*   Updated: 2022/07/31 23:56:10 by smeethon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,20 +17,25 @@
 # include <stddef.h>
 # include <unistd.h>
 
-int		ft_isalnum(int c);
-int		ft_isalpha(int c);
-int		ft_isascii(int x);
-int		ft_isdigit(int x);
-int		ft_isprint(int c);
-char	*ft_strchr(const char *s, int c);
-int		ft_strlen(const char *x);
-int		ft_strncmp(const char *s1, const char *s2, size_t n);
+int				ft_isalnum(int c);
+int				ft_isalpha(int c);
+int				ft_isascii(int x);
+int				ft_isdigit(int x);
+int				ft_isprint(int c);
+char			*ft_strchr(char *s, int c);
+size_t			ft_strlen(const char *x);
+int				ft_strncmp(const char *s1, const char *s2, size_t n);
 //char	*ft_strnstr(const char *big, const char *little, size_t len);
-char	*ft_strrchr(const char *s, int c);
-int		ft_tolower(int c);
-int		ft_toupper(int c);
-int		ft_atoi(const char *str);
-int		check_space(const char *c, int i);
-int		get_num(const char *str, int i);
+int				ft_tolower(int c);
+int				ft_toupper(int c);
+int				ft_atoi(const char *str);
+void			ft_bzero(void *s, size_t n);
+void			*ft_memcpy(void *dest, const void *src, size_t n);
+void			*ft_memccpy(void *dest, const void *src, int c, size_t n);
+void			*ft_memmove(void *dest, const void *src, size_t n);
+void			*ft_memchr(void *s, int c, size_t n);
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size);
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size);
+char			*ft_strrchr(char *s, int c);
 
 #endif
