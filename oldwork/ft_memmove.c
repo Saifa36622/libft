528361688e7231a26f75906693777173c9c4e4f2
Saifa Meethong <saifa36622@gmail.com>
2022-08-03 02:08:43 +0700

    why I am still add

--- oldwork/ft_memmove.c
+++ oldwork/ft_memmove.c
@@ -14,11 +14,30 @@
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
+    char	*x;
+	char	*z;
+	size_t	y;
 
+	x = (char *)src;
+	z = (char *)dst;
+	y = 0;
+	if (z > x)
+		while (len > 0)
+			z[len] = x[len];
+            len--;
+	else
+		while (y < len)
+		{
+			z[y] = x[y];
+			y++;
+		}
+	return (dst);
 }
-
+// man memmove 
 /*fuc that copy the n byte from the mem area of *src to *dst and the copying 
     takes place as though the bytes in src are first copied into a
     temporary array that does not overlap src or dest and the bytes
     are then copied from the temporary array to dest and then return the *dst 
     */
+   // line 21,22 -> convert src and dst to char 
+   // return dest -> void in void 
