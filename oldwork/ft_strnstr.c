853c8cc0b69647ebdb0ce5b09da6bcc323506662
Saifa Meethong <saifa36622@gmail.com>
2022-08-03 03:08:27 +0700

    i understand it now

--- oldwork/ft_strnstr.c
+++ oldwork/ft_strnstr.c
@@ -10,4 +10,35 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+/*#include "libft.h"
+
+strnstr(const char	*x, const char *y, size_t len)
+{
+
+}*/
+#include"libft.h"
+
+char *strnstr(const char	*big, const char *little, size_t len)
+{
+	size_t	x;
+	size_t	y;
+	size_t	z;
+	
+	x = 0;
+	y = 0;
+	if (!little[0])
+		return ((char *)big);
+	z = ft_strlen(little);
+	while (x < len && big[x])
+	{
+		if (big[x] == little[y])
+			y++;
+		else
+			y = 0;
+		if (!little[y])
+			return ((char *)big + x - y + 1);
+		x++;
+	}
+	return (NULL);
+}
+
