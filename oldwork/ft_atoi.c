772eaed20556e208d7b22708b9ecd92604d521cb
Saifa Meethong <saifa36622@gmail.com>
2022-08-03 02:03:55 +0700

    add atoi

--- oldwork/ft_atoi.c
+++ oldwork/ft_atoi.c
@@ -10,3 +10,31 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+int		ft_atoi(const char *str)
+{
+	int		x;
+	int		y;
+	int		z;
+	y = 0;
+	x = 1;
+	z = 0;
+	while (str[z] && (str[z] == '\f' || str[z] == '\t' || str[z] == ' ' ||
+			str[z] == '\n' || str[z] == '\r' || str[z] == '\v'))
+		i++;
+	if (str[z] == '-' || str[z] == '+')
+	{
+		if (str[z] == '-')
+			x = -1;
+            z++;
+	}
+	while (str[z] && str[z] >= '0' && str[z] <= '9')
+	{
+		y *= 10;
+		y += str[z] - '0';
+        z++;
+	}
+	y *= x;
+	return (y);
+}
