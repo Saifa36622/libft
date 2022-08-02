bc7285bd1e2ee86ca41aaed2f4c8eb6ffb68f56d
Saifa Meethong <smeethon@z2t10c2.42bangkok.com>
2022-08-03 02:51:15 +0700

    good bye everybody good luck =D

--- oldwork/ft_memmove.c
+++ oldwork/ft_memmove.c
@@ -6,11 +6,12 @@
 /*   By: smeethon <smeethon@student.42.fr>            +#+  +:+      +#+       */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/03 00:27:17 by smeethon          #+#    #+#             */
-/*   Updated: 2022/08/03 00:27:17 by smeethon         ###   ########.fr       */
+/*   Updated: 2022/08/03 02:50:42 by smeethon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include"libft.h"
+//#include"libft.h"
+#include <stdlib.h>
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
@@ -22,18 +23,21 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 	z = (char *)dst;
 	y = 0;
 	if (z > x)
-		while (len > 0)
+	{
+		while (len-- > 0)
 			z[len] = x[len];
-			len--;
+	}
 	else
+	{
 		while (y < len)
 		{
 			z[y] = x[y];
 			y++;
 		}
+	}
 	return (dst);
 }
-
+/*
 #include <stdio.h>
 #include <string.h>
 
@@ -47,12 +51,12 @@ int main () {
 
    return(0);
 }
-
+*/
 // man memmove 
 /*fuc that copy the n byte from the mem area of *src to *dst and the copying 
 	takes place as though the bytes in src are first copied into a
 	temporary array that does not overlap src or dest and the bytes
 	are then copied from the temporary array to dest and then return the *dst 
 	*/
-   // line 21,22 -> convert src and dst to char 
-   // return dest -> void in void 
+// line 21,22 -> convert src and dst to char 
+// return dest -> void in void 
