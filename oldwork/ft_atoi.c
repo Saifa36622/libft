ac8da2be4fc41f86f363156772b576269985abfe
Saifa Meethong <saifa36622@gmail.com>
2022-08-03 02:38:14 +0700

    add main

--- oldwork/ft_atoi.c
+++ oldwork/ft_atoi.c
@@ -39,3 +39,22 @@ int	ft_atoi(const char *str)
 	y *= x;
 	return (y);
 }
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main () {
+   int val;
+   char str[20];
+   
+   strcpy(str, "98993489");
+   val = atoi(str);
+   printf("String value = %s, Int value = %d\n", str, val);
+
+   strcpy(str, "tutorialspoint.com");
+   val = atoi(str);
+   printf("String value = %s, Int value = %d\n", str, val);
+
+   return(0);
+}
