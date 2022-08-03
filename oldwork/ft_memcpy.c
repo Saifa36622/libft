8b0a8a23dd3184c95547cb0af48266b02f215df7
Saifa Meethong <smeethon@z3t10c2.42bangkok.com>
2022-08-03 16:01:43 +0700

    wowzaaa

--- oldwork/ft_memcpy.c
+++ oldwork/ft_memcpy.c
@@ -1,20 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smeethon <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/03 15:58:45 by smeethon          #+#    #+#             */
+/*   Updated: 2022/08/03 16:01:31 by smeethon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
+//#include <stdlib.h>
 
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-    size_t x;
+	size_t	x;
 
+	x = 0;
 	if (!dst && !src)
 		return (0);
-	x = 0;
-	while (i < n)
+	while (x < n)
 	{
 		((unsigned char *)dst)[x] = ((unsigned char *)src)[x];
 		x++;
 	}
 	return (dst);
 }
-
+/*
 #include <stdio.h>
 #include <string.h>
 
@@ -27,7 +40,7 @@ int main () {
    printf("After memcpy dest = %s\n", dest);
    
    return(0);
-}
+}*/
 /*
     a func that cpy n byte from the mem area of src to mem area of dst 
     return -> fuch return dst
