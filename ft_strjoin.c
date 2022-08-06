8cc08a291ba162840ad3207d4cd0874723118b02
Saifa Meethong <saifa36622@gmail.com>
2022-08-06 21:37:13 +0700

    al

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -1,6 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smeethon <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/06 21:31:18 by smeethon          #+#    #+#             */
+/*   Updated: 2022/08/06 21:34:57 by smeethon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
-char    *ft_substr(char const *s, unsigned int start, size_t len)
+char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*str;
 	size_t	x;
@@ -8,7 +20,7 @@ char    *ft_substr(char const *s, unsigned int start, size_t len)
 
 	x = 0;
 	y = 0;
-	str = (char*)malloc(sizeof(*s) * (len + 1));
+	str = (char *)malloc(sizeof(*s) * (len + 1));
 	if (!str)
 		return (NULL);
 	while (s[x])
