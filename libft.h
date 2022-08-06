8d5c5558269eb1e6db9a93d75ca42062465d0f69
Saifa Meethong <saifa36622@gmail.com>
2022-08-06 21:30:50 +0700

    wfh

--- libft.h
+++ libft.h
@@ -46,5 +46,6 @@ void			ft_putchar_fd(char c, int fd);
 void			ft_putendl_fd(char *x, int fd);
 void			ft_putstr_fd(char *x, int fd);
 void			ft_putnbr_fd(int n, int fd);
+char    *ft_substr(char const *s, unsigned int start, size_t len);
 
 #endif
