72a16f62ed1f8370c0c98284620310aef90c119f
Saifa Meethong <smeethon@z3t5c1.42bangkok.com>
2022-08-26 15:21:27 +0700

    bonus

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -6,13 +6,13 @@
 /*   By: smeethon <smeethon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/26 14:27:13 by smeethon          #+#    #+#             */
-/*   Updated: 2022/08/26 15:15:58 by smeethon         ###   ########.fr       */
+/*   Updated: 2022/08/26 15:20:32 by smeethon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void ft_lstdelone(t_list *lst, void (*del)(void *))
+void	ft_lstdelone(t_list *lst, void (*del)(void *))
 {
 	if (lst)
 	{
